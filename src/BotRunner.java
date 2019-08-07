3bdab734068645b6ee00fb610ee7368919ede7ad
nglaze00 <nglaze00@gmail.com>
2019-08-06 23:34:25 -0500

    First commit

---
+++ src/BotRunner.java
@@ -0,0 +1,15 @@
+/**
+ * TODO:
+ * 	-DM new users for steam profiles 
+ * 	-Database for storing elos
+ * 	-12 players in waiting --> match players, make locked RED / BLU channels, move gamers to channels
+ * 	-Match ends, playe
+ *
+ */
+
+
+public class BotRunner {
+	public static void main(String[]args) {
+		System.out.println("my dick is warm");
+	}
+}
