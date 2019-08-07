1978696588c400127d2e7973911cc5391346f155
nglaze00 <nglaze00@gmail.com>
2019-08-06 23:54:26 -0500

    made shitty todo

---
+++ src/PugRunner.java
@@ -0,0 +1,16 @@
+/**
+ * TODO:
+ * 	-DM new users for steam profiles (when they join queue; move out of queue until they do)
+ * 	-Database for storing elos 
+ * 	-12 players in queue --> match players based on elo, make locked RED / BLU channels, move gamers to channels
+ * 	-Match ends, player ELOs updated (daily?; scan logs.tf)
+ * 		-Kick the boys out (back to chilling; players manually move themselves to queue)
+ *
+ */
+
+
+public class PugRunner {
+	public static void main(String[]args) {
+		System.out.println("my dick is warm");
+	}
+}
